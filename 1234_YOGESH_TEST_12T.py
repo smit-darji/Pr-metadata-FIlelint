131160a979aedf8691bacf500561fdb699758907
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-19 11:28:54 +0530

    first commit

--- 1234_YOGESH_TEST_12T.py
+++ 1234_YOGESH_TEST_12T.py
@@ -2,11 +2,11 @@ from fileinput import filename
 import re
 import os
 from itertools import filterfalse
-changed_files = (os.environ.get('CHANGED_FILES'))
-changed_file_list = changed_files.split(" ")
-print("changed_file_list :", changed_file_list)
+# changed_files = (os.environ.get('CHANGED_FILES'))
+# changed_file_list = changed_files.split(" ")
+# print("changed_file_list :", changed_file_list)
 file_names_to_ignore = ["README.md", ".gitignore", "dist", "images"]
-# changed_file_list = ['./.github/workflows/1.yml', './.abcd/workflows/.github/file_name_validation.sh', './jdhf/.github/workflows/test.yml', './1234_YOGESH_TEST_12T.py', './ABCD/1234_YOGESH_TEST_12T.txt']
+changed_file_list = ['./.github/workflows/2.yml', './1234_YOGESH_TEST_12T.py', './QABCD/121212121212', './QABCD/sadasdadsasdada', './QABCD/sjdfhsafd']
 directory_names_to_ignore_completely = [".github", "Terraform",".gitignore"]
 directory_names_to_ignore_list = []
 
@@ -16,7 +16,9 @@ for i in changed_file_list:
         tempData = "/".join(i.split("/", 2)[:2])
         if j in tempData:
             remove_dir_name_list.append(i)
-        
+  
+changed_file_list = [i for i in changed_file_list if i not in remove_dir_name_list ]
+print("CHANGED FILE LIST is After Ignored Dir name:", str(changed_file_list))        
 print("remove_dir_name_list:", remove_dir_name_list)
 
 unique_file_names=[]
