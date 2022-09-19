9bddc7d7c9d126a8b9d8cd559c6abf2d7badc51f
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-19 19:59:38 +0530

    first commit

--- github_actions.py
+++ github_actions.py
@@ -1,7 +1,9 @@
+import os
 from github import Github
 
+GITHUB_TOKEN = (os.environ.get('GITHUB_TOKEN'))
 def post_pr_comment (github_client, invalid_file_names, invalid_directory_names):
-  gh = Github("access_token")
+  gh = Github(GITHUB_TOKEN)
   repo = gh.repository(user, repo_name)
   pr = repo.create_pull(description, base, from_branch, detailed)
   issue = repo.issue(pr.number)
