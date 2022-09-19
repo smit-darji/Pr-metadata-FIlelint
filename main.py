58fbb9fae7dd686d586b89f2b8d4e0d1200e5444
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-19 12:31:22 +0530

    first commit

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ invalid_file_names = get_invalid_file_names(file_names_to_verify, FILE_NAMES_TO_
 
 invalid_directory_names = get_invalid_directory_names(file_names_to_verify, DIRECTORY_NAMES_TO_IGNORE)
 
-if invalid_file_names and invalid_directory_names:
+if not invalid_file_names and not invalid_directory_names:
     print("Invalid File is : ",invalid_file_names)
     print("Invalid Dir Name: ",invalid_directory_names)
     exit(1)
