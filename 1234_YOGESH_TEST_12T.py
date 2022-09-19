b3624b7d5f7bec420cf23bb849285105289022a0
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-19 10:51:47 +0530

    first commit

--- 1234_YOGESH_TEST_12T.py
+++ 1234_YOGESH_TEST_12T.py
@@ -2,22 +2,23 @@ from fileinput import filename
 import re
 import os
 from itertools import filterfalse
-changed_files = (os.environ.get('CHANGED_FILES'))
-changed_file_list = changed_files.split(" ")
-print("changed_file_list :", changed_file_list)
+# changed_files = (os.environ.get('CHANGED_FILES'))
+# changed_file_list = changed_files.split(" ")
+# print("changed_file_list :", changed_file_list)
 file_names_to_ignore = ["README.md", ".gitignore", "dist", "images"]
-# changed_file_list = ['./.github/workflows/1.yml', './.github/workflows/script/file_name_validation.sh', './.github/workflows/test.yml', './1234_YOGESH_TEST_12T.py', './ABCD/1234_YOGESH_TEST_12T.txt']
+changed_file_list = ['./.github/workflows/1.yml', './.abcd/workflows/.github/file_name_validation.sh', './jdhf/.github/workflows/test.yml', './1234_YOGESH_TEST_12T.py', './ABCD/1234_YOGESH_TEST_12T.txt']
 directory_names_to_ignore_completely = [".github", "Terraform",".gitignore"]
 directory_names_to_ignore_list = []
 
 remove_dir_name_list=[]
 for i in changed_file_list:
     for j in directory_names_to_ignore_completely:
-        if (j in i):
+        tempData = "/".join(i.split("/", 2)[:2])
+        if j in tempData:
             remove_dir_name_list.append(i)
-changed_file_list = [i for i in changed_file_list if i not in remove_dir_name_list ]
+        
 print("CHANGED FILE LIST is After Ignored Dir name:", str(changed_file_list))
-
+print("remove_dir_name_list:", remove_dir_name_list)
 
 unique_file_names=[]
 unique_file_name_list=[]
@@ -36,7 +37,7 @@ if len(unique_file_name_list) != 0:
         match = re.search("[0-9]{4}_[A-Z0-9_]*.[a-zA-Z]*$", file_name)
         print("FIlename",file_name)
         if match:
-            print("valid Filename:",file_name)
+             print("valid Filename:",file_name)
         else:
             print("Invalid FIle is :",file_name)
             file_name_list.append(file_name)
